a706c4f0edd064d36f91ef0e7e759b887ec15366
mrtom222 <38867467+mrtom222@users.noreply.github.com>
2022-05-24 11:26:35 +0000

    Commit6_2

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+function containsSpecialChars(str) {
+  const specialChars = `\`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~`;
+
+  const result = specialChars.split('').some(specialChar => {
+    if (str.includes(specialChar)) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return result;
+}
+
 (function () {
   const ex1 = document.getElementById('ex1_button')
   const ex1c = document.getElementById('ex1_content')
 
+  const ex2 = document.getElementById('ex2_text')
+  const ex2c = document.getElementById('ex2_content')
+  
   ex1.addEventListener("click", function () {
     var data = "";
     for(var i = 0;i<10;i++){
@@ -12,4 +29,24 @@
     ex1c.innerHTML = data;
   })
   
+  ex2.addEventListener("change", function () {
+    var data = ex2.value;
+    var err = 0;
+    err = 0;
+    if(containsSpecialChars(data)&&err==0){
+      ex2c.innerHTML = "Numer nie może zawierać znaków specjalnych";
+      err = 1;
+    }
+    if(isNaN(data)&&err==0){
+      ex2c.innerHTML = "Numer nie może zawierać liter";
+      err = 1;
+    }
+    if(data.length!=9&&err==0){
+      ex2c.innerHTML = "Długość numeru musi być równa 9";
+      err = 1;
+    }
+    if(err==0){
+      ex2c.innerHTML = "Numer telefonu jest poprawny";
+    }
+  })
 })();
