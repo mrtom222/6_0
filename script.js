6767fd94f0facd1812171f94df81ad17ce6a7ea9
mrtom222 <38867467+mrtom222@users.noreply.github.com>
2022-05-24 10:37:17 +0000

    Commit6_1

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 (function () {
-  //TODO
+  const ex1 = document.getElementById('ex1_button')
+  const ex1c = document.getElementById('ex1_content')
+
+  ex1.addEventListener("click", function () {
+    var data = "";
+    for(var i = 0;i<10;i++){
+      if(i!=0)
+        data += ",";
+      data += i.toString();
+    }
+    ex1c.innerHTML = data;
+  })
+  
 })();
